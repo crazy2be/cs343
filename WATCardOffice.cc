82712ef5b70463b5046fdc4e10e7685d55bff1b1
Quentin Brooks <yeerkkiller1@gmail.com>
2014-11-23 17:14:39 -0500

    WATCardOffice... may not work, I think the code is roughly there though.

---
+++ WATCardOffice.cc
@@ -0,0 +1,69 @@
+#include "WATCardOffice.h"
+#include "MPRNG.h"
+
+MPRNG randGen;
+
+#define dassert(arg) assert(arg)
+
+void WATCardOffice::Courier::main() {
+    while (true) {
+        WATCardOffice::Job *job = office.requestWork();
+        if (!job) break;
+        bank.withdraw(job->sid, job->amount);
+        job->card->deposit(job->amount);
+
+        //1 in 6 chance to lose it
+        if (randGen(6) == 0) {
+            //Lose it
+            delete job->card;
+            job->result.exception(new WATCardOffice::Lost());
+        } else {
+            job->result.delivery(job->card);
+        }
+        delete job;
+    }
+}
+WATCardOffice::Courier::Courier(WATCardOffice &office, Bank &bank)
+    : office(office), bank(bank) {}
+
+
+void WATCardOffice::main() { }
+
+WATCardOffice::WATCardOffice(Printer &printer, Bank &bank, int numCouriers)
+    : printer(printer), bank(bank), terminated(false) {
+    for (int ix = 0; ix < numCouriers; ix++) {
+        couriers.push_back(new Courier(*this, bank));
+    }
+}
+WATCardOffice::~WATCardOffice() {
+    terminate();
+    for (int ix = 0; ix < (int)couriers.size(); ix++) {
+        delete couriers[ix];
+    }
+}
+WATCard::FWATCard WATCardOffice::create(int sid, int amount) {
+    WATCard *card = new WATCard();
+
+    return transfer(sid, amount, card);
+}
+WATCard::FWATCard WATCardOffice::transfer(int sid, int amount, WATCard *card) {
+    WATCardOffice::Job *job = new WATCardOffice::Job(card, sid, amount);
+
+    jobs.push(job);
+
+    return job->result;
+}
+WATCardOffice::Job *WATCardOffice::requestWork() {
+    if (jobs.empty()) {
+        _Accept(create, transfer, terminated);
+    }
+    if (terminated) {
+        dassert(jobs.empty());
+        return NULL;
+    }
+    dassert(!jobs.empty());
+
+    WATCardOffice::Job *job = jobs.front();
+    jobs.pop();
+    return job;
+}
--- WATCardOffice.h
+++ WATCardOffice.h
@@ -1,20 +1,45 @@
 #pragma once
 
-#include <WATCard.h>
+#include "WATCard.h"
+#include "Bank.h"
+#include "Printer.h"
+
+#include <vector>
+#include <queue>
 
 _Task WATCardOffice {
     struct Job {                           // marshalled arguments and return future
-        Args args;                         // call arguments (YOU DEFINE "Args")
-        WATCard::FWATCard result;          // return future
-        Job(Args args) : args(args) {}
+        WATCard* card;
+        int sid;
+        int amount;
+        //Not sure we are using this properly, it might not copy as intended
+        WATCard::FWATCard result;
+        Job(WATCard* card, int sid, int amount)
+            : card(card), sid(sid), amount(amount) {}
     };
-    _Task Courier { ... };                 // communicates with bank
+    _Task Courier {
+    private:
+        void main();
+        WATCardOffice &office;
+        Bank &bank;
+    public:
+        Courier(WATCardOffice &, Bank &);
+    };
+
+    std::vector<Courier *> couriers;
+    std::queue<Job*> jobs;
+    Printer &printer;
+    Bank &bank;
+
+    bool terminated;
+    void terminate() { terminated = true; }
 
     void main();
-  public:
+public:
     _Event Lost {};                        // lost WATCard
-    WATCardOffice(Printer &prt, Bank &bank, unsigned int numCouriers );
+    WATCardOffice(Printer & prt, Bank & bank, int numCouriers );
+    ~WATCardOffice();
     WATCard::FWATCard create(int sid, int amount);
-    WATCard::FWATCard transfer(int sid, int amount, WATCard *card);
-    Job *requestWork();
+    WATCard::FWATCard transfer(int sid, int amount, WATCard * card);
+    Job * requestWork();
 };
