ecec9b359c31ff0529f45b8de7bbf22c2f427fd3
Quentin Brooks <yeerkkiller1@gmail.com>
2014-11-30 15:16:06 -0500

    Made random config, which appears to demonstrate a bug.

--- TestsMain.cc
+++ TestsMain.cc
@@ -15,27 +15,27 @@
 using namespace std;
 
 
-static void VectorsEqual(vector<PrintState>& lhs, vector<PrintState>& rhs) {
+static void VectorsEqual(vector<PrintState> &lhs, vector<PrintState> &rhs) {
     int ix = 0;
-    while(ix < (int)min(lhs.size(), rhs.size())) {
-        if(!lhs[ix].equals(rhs[ix])) {
+    while (ix < (int)min(lhs.size(), rhs.size())) {
+        if (!lhs[ix].equals(rhs[ix])) {
             printf("Values not equal at %d:\n", ix);
             printf("Should be:\t\t"); lhs[ix].print();
             printf("But was:\t\t"); rhs[ix].print();
         }
         ix++;
     }
-    if(ix < (int)lhs.size()) {
+    if (ix < (int)lhs.size()) {
         printf("Missing values:\n");
     }
-    while(ix < (int)lhs.size()) {
+    while (ix < (int)lhs.size()) {
         printf("\t"); lhs[ix++].print();
     }
 
-    if(ix < (int)rhs.size()) {
+    if (ix < (int)rhs.size()) {
         printf("Extra values:\n");
     }
-    while(ix < (int)rhs.size()) {
+    while (ix < (int)rhs.size()) {
         printf("\t"); rhs[ix++].print();
     }
 }
@@ -52,7 +52,7 @@ public:
     }
 };
 
-static void TestBank(uBaseTask& task) {
+static void TestBank(uBaseTask &task) {
     testPrinter.states.clear();
     Bank bank(1);
     {
@@ -63,22 +63,34 @@ static void TestBank(uBaseTask& task) {
     }
 
     vector<PrintState> TestBankResults {
-        PrintState(PrinterKind::BankDeposit, 0, ' ', 1), 
+        PrintState(PrinterKind::BankDeposit, 0, ' ', 1),
         PrintState(PrinterKind::BankDeposit, 0, ' ', 1),
         PrintState(PrinterKind::BankWithdraw, 0, ' ', 2)
     };
     VectorsEqual(TestBankResults, testPrinter.states);
 }
 
-void RunAllTests(uBaseTask& task) {
+void RunAllTests(uBaseTask &task) {
     TestBank(task);
 }
 
-void uMain::main() {
+Config genRandConfig() {
     Config config;
-    // TODO: soda.config should be argv[0]
-    readConfigFile("soda.config", config);
+    config.sodaCost = rand() % 10 + 1;
+    config.numStudents = rand() % 10 + 1;
+    config.maxPurchases = rand() % 10 + 1;
+    config.numVendingMachines = rand() % 10 + 1;
+    config.maxStockPerFlavour = rand() % 10 + 1;
+    config.maxShippedPerFlavour = rand() % 10 + 1;
+    config.timeBetweenShipments = rand() % 10 + 1;
+    config.parentalDelay = rand() % 10 + 1;
+    config.numCouriers = rand() % 10 + 1;
+    return config;
+}
 
+void uMain::main() {
+    Config config = genRandConfig();
+    
     Main(config);
     //RunAllTests(*this);
 }
